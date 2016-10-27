2974a9a8df0449fb0ed8f81e08229d71d707202d
Jesus <jesusluiso90@gmail.com>
2016-10-27 18:41:15 +0200

    primer proyecto de java on git

---
+++ src/ejerciciostema3/Ejercicios.java
@@ -0,0 +1,16 @@
+package ejerciciostema3;
+import java.math.MathContext;
+import java.util.Scanner;
+public class Ejercicios {
+
+	public static void main(String[] args) {
+		Scanner teclado=new Scanner (System.in);
+		
+		
+		 
+		
+		
+
+	}
+
+}
